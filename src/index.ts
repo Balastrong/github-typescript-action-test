ddc8d742d2737968f9355edcc1c624641085d9b1
Leonardo Montini <lion.48m@gmail.com>
2023-06-04 14:51:41 +0200

    Update action to label PRs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+import { getInput, setFailed } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+
+async function run() {
+  const token = getInput("gh-token");
+  const label = getInput("label");
+
+  const octokit = getOctokit(token);
+  const pullRequest = context.payload.pull_request;
+
+  try {
+    await octokit.rest.issues.addLabels({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: pullRequest!.number,
+      labels: [label],
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      setFailed(error?.message ?? "Unknown error");
+    }
+  }
+}
+
+run();
