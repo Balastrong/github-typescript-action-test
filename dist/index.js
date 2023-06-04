872159aa9a64296ef7075ab918aa856587e9e529
Leonardo Montini <lion.48m@gmail.com>
2023-06-04 18:13:14 +0200

    Add tests

--- dist/index.js
+++ dist/index.js
@@ -9793,6 +9793,7 @@ var __webpack_exports__ = {};
 var exports = __webpack_exports__;
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.run = void 0;
 const core_1 = __nccwpck_require__(2186);
 const github_1 = __nccwpck_require__(5438);
 async function run() {
@@ -9802,6 +9803,9 @@ async function run() {
     const octokit = (0, github_1.getOctokit)(token);
     const pullRequest = github_1.context.payload.pull_request;
     try {
+        if (!pullRequest) {
+            throw new Error("This action can only be run on Pull Requests");
+        }
         await octokit.rest.issues.addLabels({
             owner: github_1.context.repo.owner,
             repo: github_1.context.repo.repo,
@@ -9810,11 +9814,10 @@ async function run() {
         });
     }
     catch (error) {
-        if (error instanceof Error) {
-            (0, core_1.setFailed)((_a = error === null || error === void 0 ? void 0 : error.message) !== null && _a !== void 0 ? _a : "Unknown error");
-        }
+        (0, core_1.setFailed)((_a = error === null || error === void 0 ? void 0 : error.message) !== null && _a !== void 0 ? _a : "Unknown error");
     }
 }
+exports.run = run;
 run();
 
 })();
